099bb7f4406582dc76be74f4aed8ba3fd7a4627d
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-03-18 20:16:18 +0530

    added color for dices when on held

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import Die from "./Die";
+import { useState } from "react";
+
+export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+  const diceElements = dice.map((num) => <Die value={num.value} key={num.index} isHeld={num.isHeld}/>);
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={handleDiceRoll}>Roll</button>
+    </main>
+  );
+  function handleDiceRoll(){
+      setDice(generateAllNewDice())
+  }
+}
+
+
+function generateAllNewDice() {
+  const arr = [];
+  let val = 0;
+  for (let i=0; arr.length < 10;i++ ) {
+    val = Math.ceil(Math.random() * 6);
+    if (val == 0) {
+      continue;
+    }
+    arr.push({value: val,isHeld: false,index:i});
+  }
+  return arr;
+}
