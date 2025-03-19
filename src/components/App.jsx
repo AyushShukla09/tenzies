a9ea9f3351911f4c93da767eb33ba80c9f1f4e8e
AyushShukla09 <hbkayushshukla09@gmail.com>
2025-03-19 13:55:25 +0530

    Change color when die is held

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,49 @@ import Die from "./Die";
 import { useState } from "react";
 
 export default function App() {
+  function hold(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.index === id
+          ? { value: die.value, isHeld: !die.isHeld, index: die.index }
+          : die
+      )
+    );
+  }
+
   const [dice, setDice] = useState(generateAllNewDice());
-  const diceElements = dice.map((num) => <Die value={num.value} key={num.index} isHeld={num.isHeld}/>);
+
+  const diceElements = dice.map((num) => (
+    <Die
+      value={num.value}
+      key={num.index}
+      isHeld={num.isHeld}
+      hold={hold}
+      id={num.index}
+    />
+  ));
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
-      <button className="roll-dice" onClick={handleDiceRoll}>Roll</button>
+      <button className="roll-dice" onClick={handleDiceRoll}>
+        Roll
+      </button>
     </main>
   );
-  function handleDiceRoll(){
-      setDice(generateAllNewDice())
+  function handleDiceRoll() {
+    setDice(generateAllNewDice());
   }
 }
-
 
 function generateAllNewDice() {
   const arr = [];
   let val = 0;
-  for (let i=0; arr.length < 10;i++ ) {
+  for (let i = 0; arr.length < 10; i++) {
     val = Math.ceil(Math.random() * 6);
     if (val == 0) {
       continue;
     }
-    arr.push({value: val,isHeld: false,index:i});
+    arr.push({ value: val, isHeld: false, index: i });
   }
   return arr;
 }
